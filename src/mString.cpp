10850c0918fe076e45a7695c0aa29f2a289e74bb
Andriy Izbash <104149691+AndriyPetrovic@users.noreply.github.com>
2022-12-14 16:59:37 +0200

    Update mString.cpp

--- src/mString.cpp
+++ src/mString.cpp
@@ -1,5 +1,6 @@
 #include "mString.h"
 
+
 //===========================================
 //
 // Implementing class methods
@@ -9,18 +10,54 @@
 
 mString::mString()
 {
-    mainStr = 0;
+    mainStr = nullptr;
 }
+
+mString::mString(std::string str)
+{
+    mainStr = new char[str.size()];
+    std::copy(str.begin(), str.end(), mainStr);
+    mainStr[str.size()] = '\0';
+}
+
+mString::mString(char* str)
+{
+    mainStr = str;
+}
+
+mString::mString(const char* str)
+{
+    mainStr = _strdup(str);
+}
+
+mString::mString(mString* str)
+{
+    mainStr = str->mainStr;
+}
+
 
 mString::~mString()
 {
 }
 
+
 mString& mString::operator=(std::string str)
 {
-    mainStr = const_cast<char*>(str.c_str());
+    mainStr = new char[str.size()];
+    std::copy(str.begin(), str.end(), mainStr);
+    mainStr[str.size()] = '\0';
     return *this;
 }
+
+
+mString& mString::operator+=(std::string str)
+{
+    char* oldStr = mainStr;
+    mainStr = (char*)malloc(strlen(oldStr) + str.size());
+
+    return *this;
+}
+
 
 mString& mString::operator=(mString* str)
 {
@@ -28,11 +65,13 @@ mString& mString::operator=(mString* str)
     return *this;
 }
 
+
 mString& mString::operator=(char* str)
 {
     mainStr = str;
     return *this;
 }
+
 
 std::ostream& operator<<(std::ostream& out, const mString& myStr)
 {
