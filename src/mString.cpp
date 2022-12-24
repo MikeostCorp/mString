b8419cf21b6e26c8254caeb21a56edd8f03cfc97
mikeost <mikeostashenko@gmail.com>
2022-12-24 18:35:15 +0200

    fix default constructor, minor upgrades

--- src/mString.cpp
+++ src/mString.cpp
@@ -8,6 +8,8 @@
 
 mString::mString()
 {
+    mainStr = strdup("");
+    sizeStr = 0;
 }
 
 mString::mString(const mString& mStr) 
@@ -17,22 +19,16 @@ mString::mString(const mString& mStr)
     strcpy(this->mainStr, mStr.mainStr);
 }
 
-mString::mString(std::string str)
+mString::mString(const std::string str)
 {
     mainStr = strdup(str.c_str());
     sizeStr = str.size();
 }
 
-mString::mString(mString* str)
+mString::mString(const mString* str)
 {
     mainStr = strdup(str->mainStr);
     sizeStr = str->sizeStr;
-}
-
-mString::mString(char* str)
-{
-    mainStr = strdup(str);
-    sizeStr = strlen(str);
 }
 
 mString::mString(const char* str)
@@ -46,13 +42,6 @@ mString::~mString()
     free(mainStr);
 }
 
-mString& mString::operator=(std::string str)
-{
-    mainStr = strdup(str.c_str());
-    sizeStr = str.size();
-    return *this;
-}
-
 mString& mString::operator=(mString str)
 {
     mainStr = strdup(str.getTextChar());
@@ -60,10 +49,10 @@ mString& mString::operator=(mString str)
     return *this;
 }
 
-mString& mString::operator=(char* str)
+mString& mString::operator=(const std::string str)
 {
-    mainStr = strdup(str);
-    sizeStr = strlen(str);
+    mainStr = strdup(str.c_str());
+    sizeStr = str.size();
     return *this;
 }
 
@@ -71,14 +60,6 @@ mString& mString::operator=(const char* str)
 {
     mainStr = strdup(str);
     sizeStr = strlen(str);
-    return *this;
-}
-
-mString& mString::operator+=(std::string str)
-{
-    sizeStr += str.size();
-    this->mainStr = (char*)(realloc(this->mainStr, sizeStr + 1));
-    strcat(this->mainStr, str.c_str());
     return *this;
 }
 
@@ -90,11 +71,11 @@ mString& mString::operator+=(mString str)
     return *this;
 }
 
-mString& mString::operator+=(char* str)
+mString& mString::operator+=(const std::string str)
 {
-    sizeStr += strlen(str);
+    sizeStr += str.size();
     this->mainStr = (char*)(realloc(this->mainStr, sizeStr + 1));
-    strcat(this->mainStr, str);
+    strcat(this->mainStr, str.c_str());
     return *this;
 }
 
@@ -118,16 +99,16 @@ std::istream& operator>>(std::istream& in, mString& myStr)
     return in;
 }
 
-char mString::chartAt(int num)
+size_t mString::length()
+{
+    return sizeStr;
+}
+
+char mString::chartAt(const size_t num)
 {
     if(num < this->sizeStr) 
         return mainStr[num];
     return ' ';
-}
-
-int mString::length()
-{
-    return sizeStr;
 }
 
 char* mString::getTextChar()
@@ -138,7 +119,7 @@ char* mString::getTextChar()
 mString mString::toUpStr()
 {
     mString upStr = *this;
-    for(size_t i = 0; i < upStr.sizeStr; i++)
+    for(size_t i = 0; i < upStr.sizeStr; ++i)
     {
         if (upStr.mainStr[i] >= 'a' && upStr.mainStr[i] <= 'z')
         {
@@ -151,7 +132,7 @@ mString mString::toUpStr()
 mString mString::toLowStr()
 {
     mString lowStr = *this;
-    for (size_t i = 0; i < lowStr.sizeStr; i++) 
+    for (size_t i = 0; i < lowStr.sizeStr; ++i) 
     {
         if (lowStr.mainStr[i] >= 'A' && lowStr.mainStr[i] <= 'Z')
         {
@@ -161,23 +142,23 @@ mString mString::toLowStr()
     return lowStr;
 }
 
-bool mString::beginWith(char bChar)
+bool mString::beginWith(const char bChar)
 {
     if (mainStr[0] == bChar)
         return true;
     return false;
 }
 
-bool mString::endWith(char eChar)
+bool mString::endWith(const char eChar)
 {
-    if (mainStr[this->sizeStr - 1] == eChar)
+    if (mainStr[sizeStr - 1] == eChar)
         return true;
     return false;
 }
 
-bool mString::includes(char iChar)
+bool mString::includes(const char iChar)
 {
-    for (size_t i = 0; i < this->sizeStr; i++)
+    for (size_t i = 0; i < sizeStr; ++i)
     {
         if (mainStr[i] == iChar)
             return true;
@@ -185,9 +166,9 @@ bool mString::includes(char iChar)
     return false;
 }
 
-int mString::indexChar(char iChar)
+size_t mString::indexChar(const char iChar)
 {
-    for (size_t i = 0; i < this->sizeStr; i++)
+    for (size_t i = 0; i < sizeStr; ++i)
     {
         if (mainStr[i] == iChar)
             return i;
@@ -195,20 +176,20 @@ int mString::indexChar(char iChar)
     return 0;
 }
 
-int mString::lastIndexChar(char lChar)
+size_t mString::lastIndexChar(const char lChar)
 {
-    for (size_t i = 0; i < this->sizeStr; i++)
+    for (size_t i = sizeStr - 1; i >= 0; --i)
     {
-        if (mainStr[this->sizeStr - i] == lChar)
-            return this->sizeStr - i;
+        if (mainStr[i] == lChar)
+            return i;
     }
     return 0;
 }
 
-size_t mString::searchCount(char sChar)
+size_t mString::searchCount(const char sChar)
 {
     size_t countChar = 0;
-    for (size_t i = 0; i < this->sizeStr; i++)
+    for (size_t i = 0; i < sizeStr; ++i)
     {
         if (mainStr[i] == sChar)
             countChar++;
--- src/mString.h
+++ src/mString.h
@@ -15,33 +15,30 @@ public:
     //
     //=============================
 
-    mString();                    // default constuctor
-    mString(std::string str);     // constructor for string
-    mString(mString* str);        // constructor for mString
-    mString(char* str);           // constructor for char
-    mString(const char* str);     // constructor for const char
-    mString(const mString& mStr); // constructor for copy
-    ~mString();                   // destructor
+    mString();                      // default constuctor
+    mString(const std::string str); // constructor for string
+    mString(const mString* str);    // constructor for mString
+    mString(const char* str);       // constructor for const char
+    mString(const mString& mStr);   // constructor for copy
+    ~mString();                     // destructor
 
 
-    //=============================================
+    //==============================================
     // 
     // Overload operators
     //
-    //=============================================
+    //==============================================
 
-    mString& operator = (std::string str);       // overload operator for just string =
-    mString& operator = (mString str);           // overload operator for string myString =
-    mString& operator = (char* str);             // overload operator for char string =
-    mString& operator = (const char* str);       // overload operator for const char =
+    mString& operator = (mString str);            // overload operator for just myString =
+    mString& operator = (const std::string str);  // overload operator for just string =
+    mString& operator = (const char* str);        // overload operator for const char =
 
-    mString& operator += (std::string str);      // overload operator for just string +=
-    mString& operator += (mString str);          // overload operator for just mString +=
-    mString& operator += (char* str);            // overload operator for char string +=
-    mString& operator += (const char* str);      // overload operator for const char +=
+    mString& operator += (mString str);           // overload operator for just mString +=
+    mString& operator += (const std::string str); // overload operator for just string +=
+    mString& operator += (const char* str);       // overload operator for const char +=
 
 
-    //===========================================================================
+    //============================================================================
     //
     // Class friends
     //
@@ -51,21 +48,21 @@ public:
     friend std::istream& operator>> (std::istream& in, mString& myStr);         // cin myString
 
 
-    //=================================
+    //========================================
     //
     // Methods for myString
     //
-    //=================================
+    //========================================
 
-    char chartAt(int num);           // returns the character at the given number
-    int length();                    // string length
-    mString toUpStr();               // converts a string to uppercase
-    mString toLowStr();              // converts string to lower case
-    char* getTextChar();             // returns a string of type char
-    bool beginWith(char bChar);      // returns true or false depending on which given character the string starts with
-    bool endWith(char eChar);        // returns true or false depending on the given character at the end of the string
-    bool includes(char iChar);       // returns true or false depending on whether the string includes the given character
-    int indexChar(char iChar);       // return the first available character index in the string. Returns 0 if the character is not found in the string
-    int lastIndexChar(char lChar);   // returns the last available character index in the string.Returns 0 if the character is not found in the string
-    size_t searchCount(char sChar);  // returns the number of repetitions of a character in a string
+    size_t length();                        // string length
+    char chartAt(const size_t num);         // returns the character at the given index
+    mString toUpStr();                      // converts a string to uppercase
+    mString toLowStr();                     // converts a string to lower case
+    char* getTextChar();                    // returns a string of type char
+    bool beginWith(const char bChar);       // returns true or false depending on which given character the string starts with
+    bool endWith(const char eChar);         // returns true or false depending on the given character at the end of the string
+    bool includes(const char iChar);        // returns true or false depending on whether the string includes the given character
+    size_t indexChar(const char iChar);     // return the first available character index in the string. Returns 0 if the character is not found in the string
+    size_t lastIndexChar(const char lChar); // returns the last available character index in the string. Returns 0 if the character is not found in the string
+    size_t searchCount(const char sChar);   // returns the number of repetitions of a character in a string
 };
